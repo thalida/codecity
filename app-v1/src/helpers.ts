dc89e889d1b90dc02afffa9f7fa3f9a87ef23187
Thalida Noel <thalida.c.noel@gmail.com>
2022-06-02 22:58:57 -0400

    Remove app v1

--- app-v1/src/helpers.ts
+++
@@ -1,9 +0,0 @@
-export function sumObj(obj: any): number {
-  const arr: number[] = Object.values(obj);
-  return sumArr(arr);
-}
-
-export function sumArr(arr: number[]): number {
-  const sum: number = arr.reduce((p, c) => p + c, 0);
-  return sum;
-}
