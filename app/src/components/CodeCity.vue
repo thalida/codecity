8859606ab23c1f3fb63842ecb7f9379d3f40f531
Thalida Noel <thalida.c.noel@gmail.com>
2022-06-03 21:10:38 -0400

    new algo v1

--- app/src/components/CodeCity.vue
+++ app/src/components/CodeCity.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
-import groupBy from "lodash-es/groupBy";
-import filter from "lodash-es/filter";
 import axios from "axios";
-import { forEach } from "lodash-es";
-// import RenderRepo from "./RenderRepo.vue";
+import { cloneDeep } from "lodash-es";
+import RenderGrid from "./RenderGrid.vue";
 
 const props = defineProps(['repoUrl'])
 const repoData = await fetchRepo();
 const repoTree = repoData.tree;
+console.log(repoTree);
 const dirsByDepth = getDirsByDepth(repoTree);
-const maxDepth = Object.keys(dirsByDepth).length - 1;
-const dirRoads = getDirGrids(repoTree, dirsByDepth, maxDepth);
-// doThis();
+// const maxDepth = Object.keys(dirsByDepth).length - 1;
+const maxDepth = 3;
+const dirGrids = getDirGrids(repoTree, dirsByDepth, maxDepth);
+combineGrids();
 
-console.log(repoTree, dirsByDepth, dirRoads);
+const rootGrid = dirGrids['.'].grid;
+
+// console.log(rootGrid);
 
 async function fetchRepo() {
   const loc = window.location;
@@ -42,7 +44,7 @@ function getDirsByDepth(tree) {
 }
 
 function getDirGrids(repoTree, dirsByDepth, maxDepth) {
-  const dirRoads: any = {};
+  const dirGrids: any = {};
   for (let d = maxDepth; d >= 0; d -= 1) {
     const dirs = dirsByDepth[d];
     const numDirs = dirs.length;
@@ -51,24 +53,32 @@ function getDirGrids(repoTree, dirsByDepth, maxDepth) {
       const dirPath = dirs[i];
       const dir = repoTree[dirPath];
       const dirGrid: any = {};
+      const dirIntersections: any = {};
 
 
-      dirGrid[0] = { 0: ['C'] };
+      dirGrid[0] = { 0: ['S', dirPath] };
 
       let intersectionOffset = 1;
       for (let j = 0; j < dir.tree_stats.num_children; j += 1) {
         const child = repoTree[dir.child_paths[j]];
-        const gridX = Math.floor(j / 2) + intersectionOffset;
+        let gridX = Math.floor(j / 2) + intersectionOffset;
         const gridY = j % 2 === 0 ? 1 : -1;
 
         if (child.type === "blob") {
+          if (dirGrid[gridX] && dirGrid[gridX][0][0] === 'C') {
+            intersectionOffset += 1;
+            gridX += 1;
+          }
+
           if (typeof dirGrid[gridX] === 'undefined') {
             dirGrid[gridX] = {};
           }
+
           dirGrid[gridX][0] = ['R'];
           dirGrid[gridX][gridY] = ['B', child.path];
         } else {
           let shiftXBy = 0;
+
           if (dirGrid[gridX]) {
             shiftXBy += 1;
           }
@@ -78,40 +88,178 @@ function getDirGrids(repoTree, dirsByDepth, maxDepth) {
             shiftXBy += 1;
           }
 
-          dirGrid[gridX + shiftXBy] = { 0: ['C'] };
+          dirGrid[gridX + shiftXBy] = { '-1': ['C', child.path], 0: ['C', child.path], 1: ['C', child.path] };
           dirGrid[gridX + shiftXBy + 1] = { 0: ['I', child.path] };
-          dirGrid[gridX + shiftXBy + 2] = { 0: ['C'] };
+          dirGrid[gridX + shiftXBy + 2] = { '-1': ['C', child.path], 0: ['C', child.path], 1: ['C', child.path] };
+
+          dirIntersections[child.path] = {
+            x: gridX + shiftXBy + 1,
+            y: 0,
+          };
 
           intersectionOffset += 2 + shiftXBy;
         }
       }
 
-      dirRoads[dirPath] = dirGrid;
+      const maxX = Object.keys(dirGrid).length;
+      dirGrid[maxX] = { 0: ['E'] };
+
+      dirGrids[dirPath] = { grid: dirGrid, intersections: dirIntersections };
     }
   }
 
-  return dirRoads;
+  return dirGrids;
 }
 
-// function doThis() {
-//   for (let d = maxDepth; d >= 0; d -= 1) {
-//     const dirs = dirsByDepth[d];
+function combineGrids() {
+  for (let d = maxDepth; d > 0; d -= 1) {
+    const dirs = dirsByDepth[d];
 
-//     for (let i = 0; i < dirs.length; i += 1) {
-//       const dirPath = dirs[i];
-//       const childNode = repoTree[dirPath];
-//       const parentNode = repoTree[childNode.parent_path];
+    for (let i = 0; i < dirs.length; i += 1) {
+      const childPath = dirs[i];
+      const childNode = repoTree[childPath];
+      const parentNode = repoTree[childNode.parent_path];
+      const parentGrid = dirGrids[parentNode.path];
+      console.log(childPath, '=>', parentNode.path);
+      const newParentGrid = addChildToParent(childPath, parentGrid);
 
-//       const childGrid: any = {};
-//       const parentGrid: any = {};
-//     }
-//   }
-// }
+      parentGrid.grid = newParentGrid;
+    }
+  }
+}
+
+function addChildToParent(childPath, parentGrid, branchDirection: number = 1, fx: null | number = null, fy: null | number = null, shiftLeft = true) {
+  const childGrid = dirGrids[childPath];
+  const intersection = parentGrid.intersections[childPath];
+  const tmpNewParentGrid: any = {};
+
+  const ox = (fx !== null) ? fx : intersection.x;
+  const oy = (fy !== null) ? fy : intersection.y + branchDirection;
+
+  let isInvalidOrigin = false;
+  let wasInvalidAt: any = {};
+
+  for (const x in parentGrid.grid) {
+    tmpNewParentGrid[x] = {};
+
+    for (const y in parentGrid.grid[x]) {
+      tmpNewParentGrid[x][y] = parentGrid.grid[x][y].slice();
+    }
+  }
+
+  for (const x in childGrid.grid) {
+    if (!childGrid.grid.hasOwnProperty(x)) {
+      continue;
+    }
+
+    const ty = oy + (parseInt(x, 10) * branchDirection);
+
+    for (const y in childGrid.grid[x]) {
+      if (!childGrid.grid[x].hasOwnProperty(y)) {
+        continue;
+      }
+
+      const tx = ox - (parseInt(y, 10) * branchDirection);
+
+      const occupied = typeof tmpNewParentGrid[tx] !== 'undefined' && typeof tmpNewParentGrid[tx][ty] !== 'undefined';
+      if (occupied) {
+        isInvalidOrigin = true;
+        wasInvalidAt = { x: tx, y: ty, entity: tmpNewParentGrid[tx][ty] };
+        break;
+      }
+
+      if (typeof tmpNewParentGrid[tx] === 'undefined') {
+        tmpNewParentGrid[tx] = {};
+      }
+
+      tmpNewParentGrid[tx][ty] = childGrid.grid[x][y];
+    }
+
+
+    if (isInvalidOrigin) {
+      break;
+    }
+  }
+
+  if (isInvalidOrigin) {
+    const newBranchDirection = branchDirection * -1;
+    if (newBranchDirection === -1) {
+      return addChildToParent(childPath, parentGrid, newBranchDirection, ox, -1, shiftLeft);
+    }
+
+    console.log(childPath, '|', isInvalidOrigin, wasInvalidAt, '|', ox, oy, newBranchDirection);
+    // return parentGrid.grid;
+    const shiftedParentGrid: any = {};
+    const shiftedIntersections: any = {};
+    const crosswalk = (shiftLeft) ? intersection.x - 1 : intersection.x + 2;
+
+    for (const x in parentGrid.grid) {
+      if (!parentGrid.grid.hasOwnProperty(x)) {
+        continue;
+      }
+
+      const intX = parseInt(x, 10);
+
+      if (intX < crosswalk) {
+        shiftedParentGrid[intX] = parentGrid.grid[x];
+
+        if (shiftedParentGrid[intX][0] && shiftedParentGrid[intX][0][0] === 'I') {
+          const path = shiftedParentGrid[intX][0][1];
+          shiftedIntersections[path] = {
+            x: intX,
+            y: 0,
+          };
+        }
+
+        continue
+      }
+
+      if (intX === crosswalk) {
+        shiftedParentGrid[intX] = { 0: ['R'] };
+      }
+
+      const shiftedX = intX + 1;
+      // if (!shiftLeft) {
+      //   console.log(childPath, crosswalk, '|', intX, shiftedX);
+      // }
+
+      shiftedParentGrid[shiftedX] = {};
+      for (const y in parentGrid.grid[x]) {
+        if (!parentGrid.grid[x].hasOwnProperty(y)) {
+          continue;
+        }
+
+        const intY = parseInt(y, 10);
+
+        if (Math.abs(intY) > 1) {
+          shiftedParentGrid[intX][intY] = parentGrid.grid[x][y];
+          continue;
+        }
+
+        shiftedParentGrid[shiftedX][intY] = parentGrid.grid[x][y];
+
+        if (shiftedParentGrid[shiftedX][intY][0] === 'I') {
+          const path = shiftedParentGrid[shiftedX][intY][1];
+          shiftedIntersections[path] = {
+            x: shiftedX,
+            y: intY,
+          };
+        }
+      }
+    }
+
+    const nextX = (shiftLeft) ? ox + 1 : ox;
+
+    return addChildToParent(childPath, { grid: shiftedParentGrid, intersections: shiftedIntersections }, newBranchDirection, nextX, 1, !shiftLeft);
+  }
+
+  return tmpNewParentGrid;
+}
 
 </script>
 
 <template>
   <div>
-    <!-- <RenderRepo :repo="repoCity" /> -->
+    <RenderGrid :grid="rootGrid" />
   </div>
 </template>
---
+++ app/src/components/RenderGrid.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import "aframe";
+import "aframe-orbit-controls";
+import "aframe-gradient-sky";
+import "aframe-simple-sun-sky";
+
+// import "../environment.js";
+
+import { onMounted } from "vue";
+
+const props = defineProps(['grid'])
+
+const orbitCtrlTarget = {
+  x: 0,
+  y: 0.5,
+  z: 0,
+};
+const orbitCtrlTargetStr = `${orbitCtrlTarget.x} ${orbitCtrlTarget.y} ${orbitCtrlTarget.z}`;
+
+const orbitCtrlInitalPos = {
+  x: 0,
+  y: 30,
+  z: 0,
+};
+const orbitCtrlInitalPosStr = `${orbitCtrlInitalPos.x} ${orbitCtrlInitalPos.y} ${orbitCtrlInitalPos.z}`;
+
+const orbitControls = `target: ${orbitCtrlTargetStr}; initialPosition: ${orbitCtrlInitalPosStr}; minDistance: 1; maxPolarAngle: 180;`;
+
+const directionalLight = `type: directional; color: #FFF; intensity: 0.6; castShadow: true; shadowBias: -0.001; shadowRadius: 0.1; shadowCameraFar: 1000; shadowCameraFov: 90; shadowCameraTop: 1000; shadowCameraRight: 1000; shadowCameraBottom: -1000; shadowCameraLeft: -1000; shadowRadius: 0`;
+
+const directionalLightPos = {
+  x: -256,
+  y: 512,
+  z: -256,
+};
+const directionalLightPosStr = `${directionalLightPos.x} ${directionalLightPos.y} ${directionalLightPos.z}`;
+
+// const floorRadius = (props.repo.render.dimensions.width / 2) + (props.repo.render.dimensions.depth / 2);
+// const environmentSettings = `preset:forest; playArea: ${floorRadius}`;
+onMounted(() => {
+  document.querySelector('a-scene').flushToDOM(true);
+});
+</script>
+
+<template>
+  <div>
+    <a-scene shadow="type: pcfsoft">
+      <a-simple-sun-sky :sun-position="directionalLightPosStr"></a-simple-sun-sky>
+      <a-entity light="type: ambient; color: #BBB"></a-entity>
+      <a-entity camera look-controls wasd-controls :orbit-controls="orbitControls"></a-entity>
+      <a-entity :light="directionalLight" :position="directionalLightPosStr"></a-entity>
+      <!-- <a-cone position="0 -150 0" color="green" shadow="cast:false; receive: true" :height="300"
+        :radius-top="floorRadius" :radius-bottom="floorRadius / 3" segments-radial="16" material="side: double;">
+      </a-cone> -->
+
+      <!-- <a-entity :environment="environmentSettings"></a-entity> -->
+      <!-- <a-entity camera look-controls wasd-controls :orbit-controls="orbitControls"></a-entity> -->
+
+
+      <!-- <RenderDirectory :node="props.repo" /> -->
+      <a-entity v-for="(ys, x) in grid">
+        <a-entity v-for="(entity, y) in ys" :position="`${x} 0 ${y}`">
+          <a-box v-if="entity[0] === 'S' && entity[1] == '.'" color="green" :height="10" :width="1" :depth="1"></a-box>
+          <a-box v-else-if="entity[0] === 'S'" color="green" :height="1" :width="1" :depth="1">
+            <a-text :value="entity[1]"></a-text>
+          </a-box>
+          <a-box v-else-if="entity[0] === 'R'" color="gray" :height="1" :width="1" :depth="1"></a-box>
+          <a-box v-else-if="entity[0] === 'C'" color="white" :height="1" :width="1" :depth="1"></a-box>
+          <a-box v-else-if="entity[0] === 'I'" color="red" :height="1" :width="1" :depth="1"></a-box>
+          <a-box v-else-if="entity[0] === 'B'" color="blue" :height="1" :width="1" :depth="1"></a-box>
+          <a-box v-else-if="entity[0] === 'E'" color="yellow" :height="1" :width="1" :depth="1"></a-box>
+        </a-entity>
+      </a-entity>
+    </a-scene>
+  </div>
+</template>
